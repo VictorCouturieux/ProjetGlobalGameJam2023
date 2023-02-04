2c4a82992142e1440c7f197a60e409378aeaefea
Lamaxelle <axelle.desquilbet@laposte.net>
2023-02-04 13:55:11 +0100

    Début d'ui et Ui qui regarde la cam orthographique

---
+++ Assets/Scripts/uiLookAtCam.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class uiLookAtCam : MonoBehaviour
+{
+    public Camera myCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (myCam.orthographic && transform.rotation != myCam.transform.rotation) 
+        {
+            transform.rotation = myCam.transform.rotation;
+        }
+    }
+}
