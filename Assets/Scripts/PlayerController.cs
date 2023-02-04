a7ec012560f2a4fed01c9b96373b4515c21c911a
Leo <leo.db40@gmail.com>
2023-02-04 21:19:06 +0100

    progress bar not working

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,10 +12,15 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float shootingNormaliseDirection = 1;
 
     private Vector2 _inputVector;
-    private float _bulletTimer;
+    private float _startPressTimer;
     private Rigidbody _rigidbody;
 
     private PlayerPickUp _playerPickUp;
+
+    private bool _isPressingFire = false;
+    private float _timerPressLength;
+
+    private Coroutine PickUpUI;
 
     private void Awake()
     {
@@ -35,19 +40,42 @@ public class PlayerController : MonoBehaviour
 
     public void Fire(InputAction.CallbackContext context)
     {
-        Debug.Log("Fire");
-        if (!_playerPickUp.HasPlant())
-            return;
-
         if (context.started)
-            _bulletTimer = Time.time;
+        {
+            PickUpUI = StartCoroutine(PickUpUICoroutine());
+        }
 
         if (context.canceled)
         {
+            _isPressingFire = false;
+            _playerPickUp.TryCancelPickUp();
+            _playerPickUp.TryCancelThrow();
+            StopCoroutine(PickUpUI);
+            _playerPickUp.Update(0);
+        }
+    }
+
+    public void ThrowPlant()
+    {
+        if (_playerPickUp.IsHoldingPlant())
+        {
             Plant plant = _playerPickUp.GetPlant();
-            Vector3 bulletForce = Mathf.Clamp(Time.time - _bulletTimer, 0f, 3f) * (shootingNormaliseDirection*Vector3.right + Vector3.up) * 10f;
+            Vector3 bulletForce = Mathf.Clamp(_timerPressLength, 0f, 3f) * (shootingNormaliseDirection * Vector3.right + Vector3.up) * 10f;
             plant.Launch(bulletForce);
         }
+    }
+
+    private IEnumerator PickUpUICoroutine()
+    {
+        _timerPressLength = 0f;
+        while(_isPressingFire && _timerPressLength < 1f)
+        {
+            _playerPickUp.Update(_timerPressLength);
+            yield return new WaitForSeconds(Time.deltaTime);
+            _timerPressLength += Time.deltaTime;
+        }
+
+        _playerPickUp.Interact();
     }
 
     public Transform getHandTransform()
--- Assets/Scripts/PlayerPickUp.cs
+++ Assets/Scripts/PlayerPickUp.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class PlayerPickUp : MonoBehaviour
 {
+    [SerializeField] private InteractionWidget _interactionWidget;
+
     private PlayerController _playerController;
 
+    private Plant _plantPlayerCanInteractWith;
     private Plant _plant;
 
     private void Awake()
@@ -32,30 +35,93 @@ public class PlayerPickUp : MonoBehaviour
         if (plant == null)
             return;
 
-        if (HasPlant())
+        if (IsHoldingPlant())
             return;
 
         if (!plant.CanBePickUp())
             return;
 
-        PickUpPlant(plant);
+        _plantPlayerCanInteractWith = plant;
+
+        _interactionWidget.Show();
     }
 
-    private void PickUpPlant(Plant plant)
+    private void OnTriggerExit(Collider other)
     {
-        _plant = plant.PickUp();
+        Plant plant = other.GetComponent<Plant>();
+
+        if (plant == null)
+            return;
+
+        if (IsHoldingPlant())
+            return;
+
+        if (_plantPlayerCanInteractWith != plant)
+            return;
+
+        _interactionWidget.Hide();
+    }
+
+    public void PickUpPlant(Plant plant)
+    {
+        _plant = _plantPlayerCanInteractWith.PickUp();
         _plant.gameObject.transform.SetParent(_playerController.getHandTransform());
         _plant.gameObject.transform.position = _playerController.getHandTransform().position;
+        _plantPlayerCanInteractWith = null;
     }
 
-    public bool HasPlant()
+    public void Update(float timer)
+    {
+        if (CanPickUpPlant())
+        {
+            Debug.Log("CAN PICK UP PLANT");
+            if (timer <= 1)
+                _interactionWidget.SetPickUpMaskValue(timer / 1);
+        }
+        else if (IsHoldingPlant())
+        {
+            Debug.Log("IS HOLDING PLANT UPDATE");
+            if (timer <= 1)
+                _interactionWidget.SetThrowMaskValue(timer / 1);
+        }
+    }
+
+    public void Interact()
+    {
+        if(CanPickUpPlant())
+            PickUpPlant(_plantPlayerCanInteractWith);
+        else if (IsHoldingPlant())
+            _playerController.ThrowPlant();
+    }
+
+    public void TryCancelPickUp()
+    {
+        if(_plantPlayerCanInteractWith == null)
+            _interactionWidget.SetPickUpMaskValue(0);
+    }
+
+    public void TryCancelThrow()
+    {
+        if (IsHoldingPlant())
+        {
+            _playerController.ThrowPlant();
+            //_interactionWidget.SetThrowMaskValue(0);
+        }
+    }
+
+    public bool IsHoldingPlant()
     {
         return _plant != null;
     }
 
+    public bool CanPickUpPlant()
+    {
+        return _plantPlayerCanInteractWith != null;
+    }
+
     public Plant GetPlant()
     {
-        if (!HasPlant())
+        if (!IsHoldingPlant())
             Debug.LogError("Error - get plant was called but plant is null");
 
         return _plant;
