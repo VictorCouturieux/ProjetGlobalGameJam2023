78f3b2338927dde4089c5a43f5caf73ac66c853e
VictorCouturieux <victor.couturieux@laposte.net>
2023-02-05 02:03:10 +0100

    Init final GameScene + creation LifeBarre event manager

---
+++ Assets/Scripts/HudManager.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HudManager : MonoBehaviour
+{
+    [SerializeField] private Image pRightLifeBarre;
+    [SerializeField] private Image pLeftLifeBarre;
+    
+    [SerializeField] private LifeCountEvent pRightLifeCountEvent;
+    [SerializeField] private LifeCountEvent pLeftLifeCountEvent;
+    
+    [SerializeField] private float lerpDuration = 3f;
+    
+    private float _pRightTimeElapsed;
+    private float _pRightStartValue = 1;
+    private float _pRightEndValue = 1;
+    private float _pRightLifeValue;
+    private IEnumerator pRightCoroutine;
+    
+    private float _pLeftTimeElapsed;
+    private float _pLeftStartValue = 1;
+    private float _pLeftEndValue = 1;
+    private float _pLeftLifeValue;
+    private IEnumerator pLeftCoroutine;
+    
+    private void OnEnable() {
+        pRightLifeCountEvent.AddCallback(PlayerRightLostHp);
+        pLeftLifeCountEvent.AddCallback(PlayerLeftLostHp);
+    }
+
+    private void PlayerRightLostHp(float newLifeValue) {
+        if (pRightCoroutine != null) {
+            StopCoroutine(pRightCoroutine);
+        }
+        _pRightTimeElapsed = 0;
+        _pRightStartValue = pRightLifeBarre.fillAmount;
+        _pRightEndValue = newLifeValue;
+        pRightCoroutine = LerpPlayRightLifeCount();
+        StartCoroutine(pRightCoroutine);
+    }
+
+    private void PlayerLeftLostHp(float newLifeValue) {
+        if (pLeftCoroutine != null) {
+            StopCoroutine(pLeftCoroutine);
+        }
+        _pLeftTimeElapsed = 0;
+        _pLeftStartValue = pLeftLifeBarre.fillAmount;
+        _pLeftEndValue = newLifeValue;
+        pLeftCoroutine = LerpPlayLeftLifeCount();
+        StartCoroutine(pLeftCoroutine);
+    }
+
+    IEnumerator LerpPlayRightLifeCount()
+    {
+        while (_pRightTimeElapsed < lerpDuration)
+        {
+            _pRightLifeValue = Mathf.Lerp(_pRightStartValue, _pRightEndValue, _pRightTimeElapsed / lerpDuration);
+            _pRightTimeElapsed += Time.deltaTime;
+            pRightLifeBarre.fillAmount = _pRightLifeValue;
+            yield return null;
+        }
+        _pRightLifeValue = _pRightEndValue;
+    }
+    
+    IEnumerator LerpPlayLeftLifeCount()
+    {
+        while (_pLeftTimeElapsed < lerpDuration)
+        {
+            _pLeftLifeValue = Mathf.Lerp(_pLeftStartValue, _pLeftEndValue, _pLeftTimeElapsed / lerpDuration);
+            _pLeftTimeElapsed += Time.deltaTime;
+            pLeftLifeBarre.fillAmount = _pLeftLifeValue;
+            yield return null;
+        }
+        _pLeftLifeValue = _pLeftEndValue;
+    }
+    
+    
+}
---
+++ Assets/Scripts/ScriptableObjects/Events/LifeCountEvent.cs
@@ -0,0 +1,6 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Life Count Event", menuName = "Game Event/Life Count Event")]
+public class LifeCountEvent : GameEvent<float> { }
