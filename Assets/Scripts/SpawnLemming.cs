17efdcba402dadf57e52b3dec439b3bf9aa3661c
VictorCouturieux <victor.couturieux@laposte.net>
2023-02-07 00:32:02 +0100

    add Lemmings

---
+++ Assets/Scripts/SpawnLemming.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SpawnLemming : MonoBehaviour
+{
+	[SerializeField] private Animator animLemming;
+	[SerializeField] private float timerValue = 20;
+	
+	private float _timer;
+	private bool _isPlayed = false;
+
+	private void Start() {
+		_timer = timerValue;
+	}
+
+	private void Update() {
+		_timer -= Time.deltaTime;
+		if (_timer <= 0f) {
+			int roll = Random.Range(1, 20);
+			if (roll == 7) {
+				animLemming.SetTrigger("Anim");
+			}
+			_timer = timerValue;
+		}
+	}
+}
