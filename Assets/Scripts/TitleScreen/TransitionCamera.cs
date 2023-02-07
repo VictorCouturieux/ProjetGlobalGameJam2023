9507595f04843fa9affc31bdad1c04644e4acf23
Pierrhum <34352456+Pierrhum@users.noreply.github.com>
2023-02-07 15:32:26 +0100

    Camera transition & Characters animation in titlescreen

---
+++ Assets/Scripts/TitleScreen/TransitionCamera.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionCamera : MonoBehaviour
+{
+    // Anim Event
+    public void DestroyCamera()
+    {
+        Destroy(gameObject);
+    }
+}
